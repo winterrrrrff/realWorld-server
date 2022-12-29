1578c16dc0561fb99603971764877f6d4a255219
Haofeng Feng <fenghaofeng@live.com>
2022-12-28 23:04:16 -0800

    updated origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'https://winterrrrrff.github.io'
 ];
 
 module.exports = allowedOrigins;
