3170ac2705b385c5543abd7bd208871f13905b69
Haofeng Feng <fenghaofeng@live.com>
2022-12-28 22:56:36 -0800

    updated vercel.json moved index.js into api folder

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const path = require('path')
 const PORT = process.env.PORT || 4000;
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const connectDB = require('./config/dbConnect');
+const corsOptions = require('../config/corsOptions');
+const connectDB = require('../config/dbConnect');
 const mongoose = require('mongoose');
 
 console.log(process.env.NODE_ENV);
@@ -18,25 +18,25 @@ app.use(cookieParser());
 
 // static route
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/', require('./routes/root'));
+app.use('/', require('../routes/root'));
 
 // user routes - for testing
-app.use('/test', require('./routes/testRoutes'));
+app.use('/test', require('../routes/testRoutes'));
 
 // user routes - for /api/users and /api/user
-app.use('/api', require('./routes/userRoutes'));
+app.use('/api', require('../routes/userRoutes'));
 
 // user routes - for profiles
-app.use('/api/profiles', require('./routes/profileRoutes'));
+app.use('/api/profiles', require('../routes/profileRoutes'));
 
 // article routes
-app.use('/api/articles', require('./routes/articleRoutes'));
+app.use('/api/articles', require('../routes/articleRoutes'));
 
 // tag route
-app.use('/api/tags', require('./routes/tagRoutes'));
+app.use('/api/tags', require('../routes/tagRoutes'));
 
 // comment routes
-app.use('/api/articles', require('./routes/commentRoutes'));
+app.use('/api/articles', require('../routes/commentRoutes'));
 
 
 
