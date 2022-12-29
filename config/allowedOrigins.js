d05faf3d2da3ce37358f3aa4d1248c9ada2d7a7f
Haofeng Feng <fenghaofeng@live.com>
2022-12-28 23:34:51 -0800

    update origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:4200',
-    'https://winterrrrrff.github.io'
+    'https://winterrrrrff.github.io',
+    'https://angular-realworld-example-app-neon.vercel.app'
 ];
 
 module.exports = allowedOrigins;
